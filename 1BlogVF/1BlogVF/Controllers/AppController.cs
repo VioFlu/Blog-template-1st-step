f5dd14632b048595d11c69d9fd4327560c74b878
Viorica Fluer <viorica.fluer@gmail.com>
2018-12-03 20:37:05 +0100

    Blog template part 1
    Blog template part 1

---
+++ 1BlogVF/1BlogVF/Controllers/AppController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _1BlogVF.Controllers
+{
+    public class AppController : Controller
+    {
+        //the name of the action should match the name of the view we earlier created Index.chtml 
+        public IActionResult Index()
+        {
+            //View() helper methpod is used to return the ViewResult, the Index.chtml page is rendereds and shown
+            return View();
+        }
+    }
+}
